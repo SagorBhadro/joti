7676e0f61c835d850304b6842a8a05c8a9d336bb
Sagor Bhadro <sagorbhadro553311@gmail.com>
2021-04-19 13:49:01 +0600

    client

---
+++ src/Components/Home/Loggin/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBz5yeCdId5IGs5nA2I2fF_jpaxeLt59jc",
+  authDomain: "indicate-design.firebaseapp.com",
+  projectId: "indicate-design",
+  storageBucket: "indicate-design.appspot.com",
+  messagingSenderId: "293282392320",
+  appId: "1:293282392320:web:8312288248819cb8b0abe5"
+};
+
+export default firebaseConfig;
